f9dae93e0191611907dc4729fca7e9611a23d4a3
maikel.chandika <maikel.chandika@ca5d3246-8b80-2995-dbe3-195a2a688ab3>
2011-01-04 04:54:00 +0000

    Perdana nih upload Baloon

---
+++ trunk/baloon/system/application/models/tmgeneral_model.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Description of tmgeneral_model
+ *
+ * @author      Maikel Chandika <mike_hellsing@yahoo.com>
+ */
+class Tmgeneral_model extends Model{
+    function Tmgeneral_model() {
+        parent::Model();
+    }
+}
+?>
---
+++ trunk/baloon/system/application/models/tmuser_model.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Description of tmuser_model
+ *
+ * @author      Maikel Chandika <mike_hellsing@yahoo.com>
+ */
+class Tmuser_model extends Model{
+    function Tmuser_model() {
+        parent::Model();
+    }
+}
+?>
